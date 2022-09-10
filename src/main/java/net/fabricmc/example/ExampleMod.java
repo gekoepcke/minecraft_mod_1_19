3130f9eae9935f961cc29809fc8fd6b6a2d5ae1c
Laurin <laurin.mayer@web.de>
2022-09-11 01:19:22 +0200

    First commit

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -15,6 +15,7 @@ public class ExampleMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		//HALLO
 
 		LOGGER.info("Hello Fabric world!");
 	}
